92774ee02e89a116a8cb3497b9fb1d3192709dbc
Lucas Rossi <lucas.lrt1992@gmail.com>
2024-04-08 21:08:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 function isPalindrome(str){
-    /* Seu código aqui */
+    const textoOriginal = str.toLowerCase().replace(/[^a-z0-9]/g, '');    
+
+    let textoReverso = "";
+
+    for (let i = textoOriginal.length - 1 ; i >=0; i--){
+        textoReverso += textoOriginal[i];
+    }
+    if (textoOriginal === textoReverso){
+        return true;
+    } 
+    return false;
 }
 
 function arrayMaxMin(arr){
-    /* Seu código aqui */
+    let maiorNum = arr[0];
+    let menorNum = arr[0];
+
+    for (i = 1; i < arr.length; i++){
+        if(arr[i] > maiorNum){
+            maiorNum = arr[i];
+        }
+        if (arr[i] < menorNum){
+            menorNum = arr[i];
+        }
+    }
+    return [menorNum, maiorNum];
 }
+
+
